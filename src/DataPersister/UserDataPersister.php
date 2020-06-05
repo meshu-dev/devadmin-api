62b76e6f35009461eaafd21a541a9600967c73f6
meshu-dev <admin@requiredev.com>
2020-06-05 10:02:33 +0100

    Fixed CodeSniffer issues

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -8,40 +8,40 @@ use App\Entity\User;
 
 class UserDataPersister implements DataPersisterInterface
 {
-	private $entityManager;
-	private $userPasswordEncoder;
+    private $entityManager;
+    private $userPasswordEncoder;
 
-	public function __construct(
-		EntityManagerInterface $entityManager,
-		UserPasswordEncoderInterface $userPasswordEncoder
-	) {
-		$this->entityManager = $entityManager;
-		$this->userPasswordEncoder = $userPasswordEncoder;
-	}
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UserPasswordEncoderInterface $userPasswordEncoder
+    ) {
+        $this->entityManager = $entityManager;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
 
-	public function supports($data): bool
-	{
-		return $data instanceof User;
-	}
+    public function supports($data): bool
+    {
+        return $data instanceof User;
+    }
 
-	public function persist($data)
-	{
-		if ($data->getPlainPassword()) {
-			$data->setPassword(
-				$this->userPasswordEncoder->encodePassword(
-					$data,
-					$data->getPlainPassword()
-				)
-			);
-			$data->eraseCredentials();
-		}
-		$this->entityManager->persist($data);
-		$this->entityManager->flush();
-	}
+    public function persist($data)
+    {
+        if ($data->getPlainPassword()) {
+            $data->setPassword(
+                $this->userPasswordEncoder->encodePassword(
+                    $data,
+                    $data->getPlainPassword()
+                )
+            );
+            $data->eraseCredentials();
+        }
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+    }
 
-	public function remove($data)
-	{
-		$this->entityManager->remove($data);
-		$this->entityManager->flush();
-	}
+    public function remove($data)
+    {
+        $this->entityManager->remove($data);
+        $this->entityManager->flush();
+    }
 }
