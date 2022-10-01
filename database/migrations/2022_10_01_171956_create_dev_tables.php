33b03ad8e3ff6aa3095a85294c148a25ddbe755f
meshu-dev <harmeshuppal@gmail.com>
2022-10-01 20:54:45 +0100

    Added files

---
+++ database/migrations/2022_10_01_171956_create_dev_tables.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('environments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('sites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('environment_id');
+            $table->string('name', 100);
+            $table->string('url');
+
+            $table->foreign('environment_id')->references('id')->on('environments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('environments');
+        Schema::dropIfExists('sites');
+    }
+}
