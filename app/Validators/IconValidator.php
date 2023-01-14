ef9ece2ef5e5622e5f4dd6df86bd1dac324cd07b
meshu-dev <harmeshuppal@gmail.com>
2023-01-14 21:22:07 +0000

    Update icon validator

--- app/Validators/IconValidator.php
+++ app/Validators/IconValidator.php
@@ -6,10 +6,11 @@ use App\Exceptions\ValidationException;
 class IconValidator extends ApiValidator
 {
     protected $existsRules = [
-        'id' => 'required|exists:App\Models\Site,id'
+        'id' => 'required|exists:App\Models\Icon,id'
     ];
 
     protected $rules = [
+        'name' => 'required|max:100',
         'url' => 'required|url|max:500'
     ];
 }
