5b1d27673e97a58e81c9ef7cc095c4c52b6288e9
meshu-dev <harmeshuppal@gmail.com>
2023-01-16 21:22:59 +0000

    Add seeder for default data

---
+++ database/seeders/DefaultSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Environment;
+use App\Models\Icon;
+
+class DefaultSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createEnvironments();
+        $this->createIcons();
+    }
+
+    private function createEnvironments()
+    {
+        $environments = ['Production', 'Testing', 'Development'];
+
+        foreach ($environments as $environment) {
+            Environment::factory()->create([
+                'name' => $environment
+            ]);
+        }
+    }
+
+    private function createIcons()
+    {
+        $icons = [
+            ['name' => 'Angular', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg'],
+            ['name' => 'React', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'],
+            ['name' => 'Vue.js', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg'],
+            ['name' => 'Next.js', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg'],
+            ['name' => 'Nuxt.js', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg'],
+            ['name' => '11ty', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eleventy/eleventy-original.svg'],
+            ['name' => 'Laravel', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg'],
+            ['name' => 'Symfony', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/symfony/symfony-original.svg'],
+            ['name' => 'Node.js', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'],
+            ['name' => 'PHP', 'url' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg']
+        ];
+
+        foreach ($icons as $icon) {
+            Icon::factory()->create([
+                'name' => $icon['name'],
+                'url' => $icon['url']
+            ]);
+        }
+    }
+}
